b8c655ed63879c2c201e12319234d1ce4155e523
LaurentS10 <Laurents.stagech@celeriteholding.com>
2024-02-17 14:47:01 +0100

    first commit

--- src/views/Profil.vue
+++ src/views/Profil.vue
@@ -102,13 +102,7 @@ export default {
                     this.user.email = userData.email;
                     this.user.phone = userData.phone;
                     this.user.photoUrl = userData.photoUrl;
-                    // Vérifier s'il existe une URL de photo dans le localStorage
-                    //const storedPhotoUrl = localStorage.getItem('userPhotoUrl');
-                    // if (storedPhotoUrl) {
-                    //     this.user.photoUrl = storedPhotoUrl;
-                    // } else {
-                    //     this.user.photoUrl = userData.photoUrl;
-                    // }
+                    
 
                 } catch (error) {
                     console.error('Erreur lors de la requête:', error);
